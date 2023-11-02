5f5d2d95502d6bf7ab52e8a5d33b83c74f879bcc
Rob Griffiths <rob@bytespider.dev>
2023-11-02 18:54:43 +0000

    componentise the library

--- bin/meross-setup.js
+++ bin/meross-setup.js
@@ -1,53 +1,78 @@
 #!/usr/bin/env node --no-warnings
 
-'use strict'
+'use strict';
 
 import pkg from '../package.json' assert { type: 'json' };
 import { program } from 'commander';
 import TerminalKit from 'terminal-kit';
 const terminal = TerminalKit.terminal;
 
-import { configureDeviceTime, configureMqttBrokers, configureWifiParameters, queryDeviceAbility, queryDeviceInformation } from '../src/api.js'
-import { Namespace } from '../src/header.js';
-import { HTTP } from '../src/http.js';
-import { SecureWifiCredentials, WifiCredentials } from '../src/wifiCredentials.js';
+import { HTTPTransport } from '../src/transport.js';
+import { Device } from '../src/device.js';
+import { WifiAccessPoint } from '../src/wifi.js';
 
 const collection = (value, store = []) => {
-    store.push(value)
-    return store
-}
+  store.push(value);
+  return store;
+};
 
 const numberInRange = (min, max) => (value) => {
-    if (value < min || value > max) {
-        throw new program.InvalidOptionArgumentError(`Value is out of range (${min}-${max})`);
-    }
-    return parseInt(value);
-}
+  if (value < min || value > max) {
+    throw new program.InvalidOptionArgumentError(
+      `Value is out of range (${min}-${max})`
+    );
+  }
+  return parseInt(value);
+};
 
 const parseIntWithValidation = (value) => {
-    const i = parseInt(value);
-    if (isNaN(i)) {
-        throw new program.InvalidOptionArgumentError(`Value should be an integer`);
-    }
+  const i = parseInt(value);
+  if (isNaN(i)) {
+    throw new program.InvalidOptionArgumentError(`Value should be an integer`);
+  }
 
-    return i;
-}
+  return i;
+};
 
 program
-    .version(pkg.version)
-    .arguments('<options>')
-    .requiredOption('-a, --ip <ip>', 'Send command to device with this IP address', '10.10.10.1')
-    .option('--wifi-ssid <wifi-ssid>', 'WIFI AP name')
-    .option('--wifi-pass <wifi-pass>', 'WIFI AP password')
-    .option('--wifi-encryption <wifi-encryption>', 'WIFI AP encryption(this can be found using meross info --include-wifi)', parseIntWithValidation)
-    .option('--wifi-cipher <wifi-cipher>', 'WIFI AP cipher (this can be found using meross info --include-wifi)', parseIntWithValidation)
-    .option('--wifi-bssid <wifi-bssid>', 'WIFI AP BSSID (each octet seperated by a colon `:`)')
-    .option('--wifi-channel <wifi-channel>', 'WIFI AP 2.5GHz channel number [1-13] (this can be found using meross info --include-wifi)', numberInRange(1, 13))
-    .option('--mqtt <mqtt-server>', 'MQTT server address', collection)
-    .option('-u, --user <user-id>', 'Integer id. Only useful for connecting to Meross Cloud.', parseIntWithValidation, 0)
-    .option('-k, --key <shared-key>', 'Shared key for generating signatures', '')
-    .option('-v, --verbose', 'Show debugging messages', '')
-    .parse(process.argv)
+  .version(pkg.version)
+  .arguments('<options>')
+  .requiredOption(
+    '-a, --ip <ip>',
+    'Send command to device with this IP address',
+    '10.10.10.1'
+  )
+  .option('--wifi-ssid <wifi-ssid>', 'WIFI AP name')
+  .option('--wifi-pass <wifi-pass>', 'WIFI AP password')
+  .option(
+    '--wifi-encryption <wifi-encryption>',
+    'WIFI AP encryption(this can be found using meross info --include-wifi)',
+    parseIntWithValidation
+  )
+  .option(
+    '--wifi-cipher <wifi-cipher>',
+    'WIFI AP cipher (this can be found using meross info --include-wifi)',
+    parseIntWithValidation
+  )
+  .option(
+    '--wifi-bssid <wifi-bssid>',
+    'WIFI AP BSSID (each octet seperated by a colon `:`)'
+  )
+  .option(
+    '--wifi-channel <wifi-channel>',
+    'WIFI AP 2.5GHz channel number [1-13] (this can be found using meross info --include-wifi)',
+    numberInRange(1, 13)
+  )
+  .option('--mqtt <mqtt-server>', 'MQTT server address', collection)
+  .option(
+    '-u, --user <user-id>',
+    'Integer id. Only useful for connecting to Meross Cloud.',
+    parseIntWithValidation,
+    0
+  )
+  .option('-k, --key <shared-key>', 'Shared key for generating signatures', '')
+  .option('-v, --verbose', 'Show debugging messages', '')
+  .parse(process.argv);
 
 const options = program.opts();
 
@@ -56,73 +81,48 @@ const key = options.key;
 const userId = options.user;
 const verbose = options.verbose;
 
-let spinner = await terminal.spinner({ rightPadding: ' ' })
+let spinner;
 try {
-    const http = new HTTP(ip);
+  spinner = await terminal.spinner({
+    animation: 'dotSpinner',
+    rightPadding: ' ',
+    attr: { color: 'cyan' },
+  });
 
-    await configureDeviceTime({
-        http,
-        key,
-        userId,
+  const transport = new HTTPTransport({ ip });
+  const device = new Device({ transport });
+
+  await device.setSystemTime();
+  terminal('• Configured Device time.\n');
+
+  const { mqtt = [] } = options;
+  if (mqtt.length) {
+    await device.configureMQTTBrokers({
+      mqtt,
+    });
+    terminal('• Configured MQTT brokers.\n');
+  }
+
+  if (options.wifiSsid && options.wifiPass) {
+    const wifiAccessPoint = new WifiAccessPoint({
+      ssid: options.wifiSsid,
+      password: options.wifiPass,
+      channel: options.wifiChannel,
+      encryption: options.wifiEncryption,
+      cipher: options.wifiCipher,
+      bssid: options.wifiBssid,
+    });
+    await device.configureWifi({
+      wifiAccessPoint,
     });
 
-    terminal("\n• Configured Device time.");
-
-    if (options.mqtt && options.mqtt.length) {
-        await configureMqttBrokers({
-            http,
-            key,
-            userId,
-            mqtt: options.mqtt
-        });
-        terminal("\n• Configured MQTT brokers.");
-    }
-
-    if (options.wifiSsid && options.wifiPass) {
-        const deviceAbility = await queryDeviceAbility({
-            http,
-            key,
-            userId,
-        });
-
-        deviceAbility[Namespace.CONFIG_WIFIX] = {};
-
-        let credentials;
-        if (Namespace.CONFIG_WIFIX in deviceAbility) {
-            const deviceInformation = await queryDeviceInformation({
-                http,
-                key,
-                userId,
-            });
-
-            credentials = new SecureWifiCredentials(options.wifiSsid, options.wifiPass);
-            credentials.encrypt({
-                ...deviceInformation.hardware
-            });
-            console.log(credentials);
-            process.exit();
-        } else {
-            credentials = new WifiCredentials(options.wifiSsid, options.wifiPass);
-        }
-
-        await configureWifiParameters({
-            http,
-            key,
-            userId,
-            parameters: {
-                credentials,
-                channel: options.wifiChannel,
-                encryption: options.wifiEncryption,
-                cipher: options.wifiCipher,
-                bssid: options.wifiBssid,
-            }
-        });
-
-        terminal("\n• Configured WIFI.");
-        terminal.green(`Device will now reboot...`);
-    }
+    terminal('• Configured WIFI.\n');
+    terminal.green(`Device will now reboot...\n`);
+  }
 } catch (error) {
-    terminal.red(error.message);
+  terminal.red(error.message);
 } finally {
+  if (spinner) {
     spinner.animate(false);
+  }
 }
--- src/message.js
+++ src/message.js
@@ -1,8 +1,9 @@
 import { createHash } from 'crypto';
-import { Header } from './header.js';
+import { Header, Method, Namespace } from './header.js';
+import { generateTimestamp, filterUndefined, base64 } from './util.js';
 
 /**
- * 
+ *
  */
 export class Message {
   header;
@@ -14,11 +15,155 @@ export class Message {
   }
 
   async sign(key = '') {
-    const {
-      messageId,
-      timestamp
-    } = this.header;
+    const { messageId, timestamp } = this.header;
 
-    this.header.sign = createHash('md5').update(`${messageId}${key}${timestamp}`).digest('hex');
+    this.header.sign = createHash('md5')
+      .update(`${messageId}${key}${timestamp}`)
+      .digest('hex');
+  }
+}
+
+export class QuerySystemInformationMessage extends Message {
+  constructor() {
+    super();
+
+    this.header.method = Method.GET;
+    this.header.namespace = Namespace.SYSTEM_ALL;
+    this.payload = {};
+  }
+}
+
+export class QuerySystemFirmwareMessage extends Message {
+  constructor() {
+    super();
+
+    this.header.method = Method.GET;
+    this.header.namespace = Namespace.SYSTEM_FIRMWARE;
+    this.payload = {};
+  }
+}
+
+export class QuerySystemHardwareMessage extends Message {
+  constructor() {
+    super();
+
+    this.header.method = Method.GET;
+    this.header.namespace = Namespace.SYSTEM_HARDWARE;
+    this.payload = {};
+  }
+}
+
+export class QuerySystemAbilityMessage extends Message {
+  constructor() {
+    super();
+
+    this.header.method = Method.GET;
+    this.header.namespace = Namespace.SYSTEM_ABILITY;
+    this.payload = {};
+  }
+}
+
+export class QuerySystemTimeMessage extends Message {
+  constructor() {
+    super();
+
+    this.header.method = Method.GET;
+    this.header.namespace = Namespace.SYSTEM_TIME;
+    this.payload = {};
+  }
+}
+
+export class SetSystemTimeMessage extends Message {
+  constructor({
+    timestamp = generateTimestamp(),
+    timezone = Intl.DateTimeFormat().resolvedOptions().timeZone,
+    timeRule = [],
+  }) {
+    super();
+
+    this.header.method = Method.SET;
+    this.header.namespace = Namespace.SYSTEM_TIME;
+    this.payload = { time: {} };
+
+    if (timestamp > 0) {
+      this.payload.time.timestamp = timestamp;
+    }
+    this.payload.time.timezone = timezone;
+    this.payload.time.timeRule = timeRule;
+  }
+}
+
+export class QueryNearbyWifiMessage extends Message {
+  constructor() {
+    super();
+
+    this.header.method = Method.GET;
+    this.header.namespace = Namespace.CONFIG_WIFI_LIST;
+    this.payload = {};
+  }
+}
+
+export class ConfigureMQTTMessage extends Message {
+  constructor({ mqtt = [], credentials } = {}) {
+    super();
+
+    this.header.method = Method.SET;
+    this.header.namespace = Namespace.CONFIG_KEY;
+
+    const brokers = mqtt
+      .map((address) => {
+        let { protocol, hostname: host, port } = new URL(address);
+        if (!port) {
+          if (protocol === 'mqtt:') {
+            port = '1883';
+          }
+          if (protocol === 'mqtts:') {
+            port = '8883';
+          }
+        }
+        return { host, port };
+      })
+      .slice(0, 2);
+
+    const firstBroker = brokers[0];
+    const secondBroker = brokers[1] ?? brokers[0];
+
+    this.payload = {
+      key: {
+        userId: `${credentials.userId}`,
+        key: credentials.key,
+        gateway: {
+          host: firstBroker.host,
+          port: Number(firstBroker.port),
+          secondHost: secondBroker.host,
+          secondPort: Number(secondBroker.port),
+          redirect: 1,
+        },
+      },
+    };
+  }
+}
+
+export class ConfigureWifiMessage extends Message {
+  constructor({ wifiAccessPoint } = {}) {
+    super();
+
+    this.header.method = Method.SET;
+    this.header.namespace = Namespace.CONFIG_WIFI;
+    this.payload = {
+      wifi: {
+        ...filterUndefined(wifiAccessPoint),
+        ssid: base64.encode(wifiAccessPoint.ssid),
+        password: base64.encode(wifiAccessPoint.password),
+      },
+    };
+  }
+}
+
+export class ConfigureWifiXMessage extends ConfigureWifiMessage {
+  constructor({ wifiAccessPoint } = {}) {
+    super({ wifiAccessPoint });
+
+    this.header.namespace = Namespace.CONFIG_WIFIX;
   }
 }
