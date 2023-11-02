dc6a381b11956a865486c225fe277f6c74075798
Rob Griffiths <rob@bytespider.dev>
2023-11-02 18:54:43 +0000

    componentise the library

--- bin/meross-info.js
+++ bin/meross-info.js
@@ -1,27 +1,43 @@
 #!/usr/bin/env node --no-warnings
 
-'use strict'
+'use strict';
 
 import pkg from '../package.json' assert { type: 'json' };
 import { program } from 'commander';
 import TerminalKit from 'terminal-kit';
 const terminal = TerminalKit.terminal;
 
-import { queryDeviceAbility, queryDeviceTime, queryDeviceInformation, queryDeviceWifiList } from '../src/api.js';
-import { HTTP } from "../src/http.js";
+import {
+  queryDeviceAbility,
+  queryDeviceTime,
+  queryDeviceInformation,
+  queryDeviceWifiList,
+} from '../src/api.js';
+import { HTTP } from '../src/http.js';
 import { printDeviceTable, printWifiListTable } from '../src/cli.js';
+import { Device } from '../src/device.js';
+import { HTTPTransport } from '../src/transport.js';
+import { WifiCipher } from '../src/wifi.js';
 
 program
-    .version(pkg.version)
-    .arguments('<options>')
-    .requiredOption('-a, --ip <ip>', 'Send command to device with this IP address', '10.10.10.1')
-    .option('-u, --user <user-id>', 'Integer id. Only useful for connecting to Meross Cloud.', parseInt)
-    .option('-k, --key <shared-key>', 'Shared key for generating signatures', '')
-    .option('--include-wifi', 'List WIFI access points near the device')
-    .option('--include-ability', 'List device ability list')
-    .option('--include-time', 'List device time')
-    .option('-v, --verbose', 'Show debugging messages')
-    .parse(process.argv)
+  .version(pkg.version)
+  .arguments('<options>')
+  .requiredOption(
+    '-a, --ip <ip>',
+    'Send command to device with this IP address',
+    '10.10.10.1'
+  )
+  .option(
+    '-u, --user <user-id>',
+    'Integer id. Only useful for connecting to Meross Cloud.',
+    parseInt
+  )
+  .option('-k, --key <shared-key>', 'Shared key for generating signatures', '')
+  .option('--include-wifi', 'List WIFI access points near the device')
+  .option('--include-ability', 'List device ability list')
+  .option('--include-time', 'List device time')
+  .option('-v, --verbose', 'Show debugging messages')
+  .parse(process.argv);
 
 const options = program.opts();
 
@@ -33,36 +49,43 @@ const includeAbilityList = options.includeAbility;
 const includeTime = options.includeTime;
 const verbose = options.verbose;
 
-console.log(`Getting info about device with IP ${ip}`)
+console.log(`Getting info about device with IP ${ip}`);
 
+let spinner;
 try {
-    const http = new HTTP(ip);
+  const transport = new HTTPTransport({ ip });
+  const device = new Device({ transport });
 
-    const deviceInformation = await queryDeviceInformation({ http });
+  const deviceInformation = await device.querySystemInformation();
 
-    let deviceAbility;
-    if (includeAbilityList) {
-        deviceAbility = await queryDeviceAbility({ http });
-    }
+  let deviceAbility;
+  if (includeAbilityList) {
+    deviceAbility = await device.querySystemAbility();
+  }
 
-    let deviceTime;
-    if (includeTime) {
-        deviceTime = await queryDeviceTime({ http });
-    }
+  let deviceTime;
+  if (includeTime) {
+    deviceTime = await device.querySystemTime();
+  }
 
-    await printDeviceTable(deviceInformation, deviceAbility, deviceTime);
+  await printDeviceTable(deviceInformation, deviceAbility, deviceTime);
 
-    if (includeWifiList) {
-        let spinner = await terminal.spinner({ animation: 'dotSpinner', rightPadding: ' ' })
-        terminal('Getting WIFI list…\n')
+  if (includeWifiList) {
+    spinner = await terminal.spinner({
+      animation: 'dotSpinner',
+      rightPadding: ' ',
+      attr: { color: 'cyan' },
+    });
+    terminal('Getting WIFI list…\n');
 
-        const wifiList = await queryDeviceWifiList({ http });
+    const wifiList = await device.queryNearbyWifi();
 
-        spinner.animate(false);
-        terminal.move(0, -1);
-
-        await printWifiListTable(wifiList);
-    }
+    await printWifiListTable(wifiList);
+  }
 } catch (error) {
-    terminal.red(error.message);
+  terminal.red(error.message);
+} finally {
+  if (spinner) {
+    spinner.animate(false);
+  }
 }
