011d83b252adf014fcace5bd1ae04fe5fa73717f
Rob Griffiths <rob@bytespider.dev>
2025-04-05 18:01:27 +0100

    decide the protocol and the port based on the input

--- packages/lib/src/utils/protocolFromPort.ts
+++ packages/lib/src/utils/protocolFromPort.ts
@@ -13,4 +13,26 @@ export function protocolFromPort(port: number) {
   throw new Error(`Unknown port ${port}`);
 }
 
-export default protocolFromPort;
+export function portFromProtocol(protocol: string) {
+  switch (protocol) {
+    case 'http':
+      return 80;
+    case 'https':
+      return 443;
+    case 'mqtts':
+      return 8883;
+    case 'mqtt':
+      return 1883;
+  }
+  throw new Error(`Unknown protocol ${protocol}`);
+}
+
+export function isValidPort(port: number) {
+  return port === 80 || port === 443 || port === 8883 || port === 1883;
+}
+
+export default {
+  protocolFromPort,
+  portFromProtocol,
+  isValidPort,
+};
