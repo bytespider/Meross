6304527e2804b1b7017640eaf925752aface2a90
Rob Griffiths <rob@bytespider.dev>
2025-08-02 17:37:29 +0100

    added missing command

---
+++ packages/cli/src/meross.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict';
+
+import pkg from '../package.json' with { type: 'json' };
+import { program } from 'commander';
+
+program
+  .version(pkg.version)
+  .command('info [options]', 'get information about compatable Meross smart device')
+  .command('setup [options]', 'setup compatable Meross smart device')
+  .parse(process.argv);
